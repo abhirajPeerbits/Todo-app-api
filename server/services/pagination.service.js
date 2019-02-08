e69a06638a5b115c1813041e123dfaa10344385c
Uzer Kagdi <Kagdi@peerbits.com>
2019-02-08 16:50:28 +0530

    intial commit

--- server/services/pagination.service.js
+++ server/services/pagination.service.js
@@ -13,6 +13,10 @@
 
 
 let pagination = (req,res) => {
+  // 
+  // let pageNo = Number(req.body.page ? req.body.page : 1);
+  // let limit = Number(req.body.limit ? Number(req.body.limit) : 10);
+  // let keyword= String(req.body.keyword ? String(req.body.keyword) : "");
 
     let pageNo = parseInt(req.query.pageNo);
     let size = parseInt(req.query.size);
